17a606c86de97fae831898fa5ac8159a976cb90f
Brian Hague <alphaseinor@gmail.com>
2020-07-25 21:31:30 -0500

    added stack with test

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,32 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        print(len(self.storage))
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        print(self.storage)
+        self.storage.append(value)
+        print(self.storage)
 
     def pop(self):
-        pass
+        if(len(self.storage) == 0):
+            return None
+        return self.storage.pop()
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
